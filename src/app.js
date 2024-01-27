8500a7acc5993049022a5cb3b6ac121d62192f5e
xmarsxx <jo@byu.net>
2024-01-27 15:27:47 -0700

    added content to readme and put base down for html and js for the app

---
+++ src/app.js
@@ -0,0 +1,84 @@
+const Scriptures = (function () {
+  "use strict";
+
+  // Constants
+
+  // Private Variables
+  let books;
+  let volumes;
+
+  // Private Method Declarations
+  let ajax;
+  let cacheBooks;
+  let init;
+
+  // Private Methods
+  ajax = function (url, successCallback, failureCallback) {
+    let request = new XMLHttpRequest();
+    request.open("GET", url, true);
+
+    request.onload = function () {
+      if (this.status >= 200 && this.status < 400) {
+        let data = JSON.parse(this.response);
+
+        if (typeof successCallback === "function") {
+          successCallback(data);
+        }
+      } else {
+        if (typeof failureCallback === "function") {
+          failureCallback(request);
+        }
+      }
+    };
+
+    request.onerror = failureCallback;
+
+    request.send();
+  };
+
+  cacheBooks = function (callback) {
+    volumes.forEach((volume) => {
+      let volumeBooks = [];
+      let bookId = volume.minBookId;
+
+      while (bookId <= volume.maxBookId) {
+        volumeBooks.push(books[bookId]);
+        bookId += 1;
+      }
+
+      volume.books = volumeBooks;
+    });
+
+    if (typeof callback === "function") {
+      callback();
+    }
+  };
+
+  init = function (callback) {
+    let booksLoaded = false;
+    let volumesLoaded = false;
+
+    ajax("https://scriptures.byu.edu/mapscrip/model/books.php", (data) => {
+      books = data;
+      booksLoaded = true;
+
+      if (volumesLoaded) {
+        cacheBooks(callback);
+      }
+    });
+    ajax("https://scriptures.byu.edu/mapscrip/model/volumes.php", (data) => {
+      volumes = data;
+      volumesLoaded = true;
+
+      if (booksLoaded) {
+        cacheBooks(callback);
+      }
+    });
+  };
+
+  // Public API
+
+  return {
+    init: init,
+  };
+})();
