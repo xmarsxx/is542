b8c9d91ef5993f1c52ed68e40248021aeae59d31
Jo Windley <jo@windley.com>
2024-02-10 14:47:36 -0700

    getting base map working

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const Scriptures = (function () {
-  "use strict";
+  ("use strict");
 
   // Constants
 
@@ -77,6 +77,35 @@ const Scriptures = (function () {
   };
 
   // Public API
+
+  // Initialize and add the map
+  let map;
+
+  async function initMap() {
+    // The location of Uluru
+    const position = { lat: -25.344, lng: 131.031 };
+    // Request needed libraries.
+    //@ts-ignore
+    const { Map } = await google.maps.importLibrary("maps");
+    const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+    // The map, centered at Uluru
+    map = new Map(document.getElementById("map"), {
+      zoom: 4,
+      center: position,
+      mapId: "DEMO_MAP_ID",
+    });
+
+    // The marker, positioned at Uluru
+    const marker = new AdvancedMarkerElement({
+      map: map,
+      position: position,
+      title: "Uluru",
+    });
+    console.log("Maps JavaScript API loaded.");
+  }
+
+  initMap();
 
   return {
     init: init,
